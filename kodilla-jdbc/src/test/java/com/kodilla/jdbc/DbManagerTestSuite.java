21bd5b314e675e8c2023a89e1cd39573460b99f9
dazz4 <kaminski.darek@icloud.com>
2019-06-21 02:24:42 +0100

    16.7

---
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -0,0 +1,67 @@
+package com.kodilla.jdbc;
+
+import com.mysql.cj.xdevapi.SqlDataResult;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.swing.plaf.nimbus.State;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbManagerTestSuite {
+    @Test
+    public void testConnection() throws SQLException {
+        DbManager dbManager = DbManager.getInstance();
+        Assert.assertNotNull(dbManager.getConnection());
+    }
+    @Test
+    public void testSelectUsers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        //When
+        String sqlQuery = "SELECT * FROM USERS";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while (rs.next()) {
+            System.out.println(rs.getInt("ID") + ", " +
+                    rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        Assert.assertEquals(5, counter);
+
+    }
+
+    @Test
+    public void testSelectUsersAndPosts() throws SQLException{
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        //When
+        String sqlQuery = "SELECT U.FIRSTNAME, U.LASTNAME, COUNT(*) POSTS_NUMBER " +
+                "FROM USERS U " +
+                "JOIN POSTS P ON P.USER_ID = U.ID " +
+                "GROUP BY P.USER_ID " +
+                "HAVING COUNT(*) >= 2;";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+        //Then
+        int counter = 0;
+        while (rs.next()) {
+            System.out.println(rs.getString("FIRSTNAME") + " " +
+                    rs.getString("LASTNAME") + " " +
+                    rs.getString("POSTS_NUMBER")
+            );
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        Assert.assertEquals(2, counter);
+
+    }
+}
